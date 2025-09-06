bdd2a7d51b199a3111d16f0207243239c99de9bd
BitterLemon-git <mihajlovs928@gmail.com>
2025-09-06 19:35:52 +0300

    Third order determinant

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io, ops::{Mul, Sub}, str::FromStr};
+use std::{io, ops::{Mul, Sub, Add}, str::FromStr};
 
 // first square brackets = line
 // second square brackets = column
@@ -39,18 +39,30 @@ fn order_determinant<T>(vec: &Vec<Vec<T>>, size: &Size) -> T
 where T:
     Copy +
     Mul<Output = T> +
-    Sub<Output = T>
+    Sub<Output = T> +
+    Add<Output = T>
 {
     match size {
         Size { width: 2, height: 2 } => (vec[0][0] * vec[1][1]) - (vec[1][0] * vec[0][1]),
         Size { width: 3, height: 3 } => {
-            (vec[0][0] * vec[1][1] * vec[2][2])
-        }
+            (vec[0][0] * vec[1][1] * vec[2][2]) +
+            (vec[0][1] * vec[1][2] * vec[2][0]) +
+            (vec[0][2] * vec[1][0] * vec[2][1]) -
+            (vec[0][1] * vec[1][0] * vec[2][2]) -
+            (vec[0][0] * vec[1][2] * vec[2][1]) -
+            (vec[0][2] * vec[1][1] * vec[2][0])
+        },
         _ => todo!(),
     }
 }
 
 fn main() -> io::Result<()> {
+    let test = vec![
+        vec![1, -2, 3],
+        vec![2, -4, 1],
+        vec![3, -5, 2]
+    ];
+
     let size = Size { width: 3, height: 3 };
     let vec: Vec<Vec<i32>> = read_matrix(&size);
 
